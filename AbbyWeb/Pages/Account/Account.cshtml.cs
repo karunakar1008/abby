05ee74f508559192411529c0436ef6c404995b4e
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-01 21:39:29 +0530

    Routing demo

---
+++ AbbyWeb/Pages/Account/Account.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Account
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
